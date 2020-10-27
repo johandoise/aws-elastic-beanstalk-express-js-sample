da179c74adf7bbd88418d2131d2d1fd0f95cbe3a
johandoise <johan.doise@gmail.com>
2020-10-27 11:06:56 +0100

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Training GIT!'));
+app.get('/', (req, res) => res.send('STAYING ALIVE!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
